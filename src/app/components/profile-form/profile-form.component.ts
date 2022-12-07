df42c5dc0d446e293986dc5b67a04459583c3ee0
MilosS019 <milosstanojlovic019@gmail.com>
2022-12-07 20:20:08 +0100

    Added a notification for successfull profile edit

--- src/app/components/profile-form/profile-form.component.ts
+++ src/app/components/profile-form/profile-form.component.ts
@@ -11,11 +11,21 @@ import { Passenger } from 'app/models/passenger.model';
 export class ProfileFormComponent{
 
   passenger = {} as Passenger;
+  notificationOffset:String = "-70px";
+  succesfull : Boolean = true;
 
   constructor(private passengerService : PassengerService) {}
 
   onSubmit(f: NgForm): void {
     // Process checkout data here
-    this.passengerService.updatePassenger(f);
+    this.succesfull = this.passengerService.updatePassenger(f);
+    this.notificationOffset = "30px";
+    setTimeout(()=>{
+      this.notificationOffset = "-70px"
+    }, 3000);
+  }
+
+  closeNotification(): void{
+    this.notificationOffset = "-70px";
   }
 }
--- src/app/services/passenger.service.ts
+++ src/app/services/passenger.service.ts
@@ -8,12 +8,17 @@ import { NgForm } from '@angular/forms';
   providedIn: 'root'
 })
 export class PassengerService {
-  private apiURL: string = 'http://localhost:4200/api/passenger/1';
+  private apiURL: string = 'http://localhost:8081/api/passenger/2'; 
 
   constructor(private http: HttpClient) {}
 
-  public updatePassenger(form : NgForm){
-    this.http.put(this.apiURL, form.value).subscribe(data => console.log(data));
+  public updatePassenger(form : NgForm): Boolean{
+    try{
+      this.http.put(this.apiURL, form.value).subscribe(data => console.log(data));
+    }catch{
+      return false;
+    }
+    return true;
   }
 }
 
