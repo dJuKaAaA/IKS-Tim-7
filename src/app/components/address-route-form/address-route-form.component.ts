84f4678cd3c82d248246e7bbd6b43841f97dde24
Ivan Djukanovic <ivan.djukanovic07@gmail.com>
2022-12-10 17:29:39 +0100

    Added address routing to unregistered user home page

--- src/app/components/address-route-form/address-route-form.component.ts
+++ src/app/components/address-route-form/address-route-form.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MapComponent } from '../map/map.component';
 
 @Component({
   selector: 'app-address-route-form',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./address-route-form.component.css']
 })
 export class AddressRouteFormComponent {
+
+  @Input() startAddress: string = "";
+  @Input() endAddress: string = "";
+  @Input() mapComponent: MapComponent;
+  @Input() callbackFunc = (): void => {};
+
+  showRoute(): void {
+    this.mapComponent.clearMap();
+    this.mapComponent.showRouteFromAddresses(this.startAddress, this.endAddress);
+    this.callbackFunc();
+  }
 
 }
--- src/app/components/unregistered-home/unregistered-home.component.ts
+++ src/app/components/unregistered-home/unregistered-home.component.ts
@@ -1,7 +1,6 @@
-import { Component, NgModule, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, NgModule, ViewChild } from '@angular/core';
 import { OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { map } from '@tomtom-international/web-sdk-maps';
 import { Location } from 'src/app/model/location.model';
 import { Route } from 'src/app/model/route.model';
 import { MapComponent } from '../map/map.component';
@@ -17,18 +16,8 @@ export class UnregisteredHomeComponent {
 
   bgImagePath: string = "../../../assets/unregistered-home-bg-img.png"
 
-  locations: Array<Location> = [
-    // new Location(45.24628246945485, 19.85170752737503, {}),
-    // new Location(45.245527089155075, 19.850516626557415, {}),
-    // new Location(45.246191824349225, 19.84923989505024, {}),
-    // new Location(45.25034547972879, 19.85242635939427, {})
-  ];
-  routes: Array<Route> = [
-    new Route(
-      new Location(45.25628246945484, 19.85170752737503, {}),
-      new Location(45.556058433301284, 19.845757083194272, {})
-    )
-  ]
+  locations: Array<Location> = [];
+  routes: Array<Route> = []
 
   startAddress: string = ""
   endAddress: string = ""
@@ -47,6 +36,7 @@ export class UnregisteredHomeComponent {
 
   showRouteForAddresses(): void {
     this.mapComponent.showRouteFromAddresses(this.startAddress, this.endAddress);
+    this.goToMaps();
   }
 
   clearMap(): void {
