0abb8bdbfbbd84cfe603a5894d74954fb103ded3
Ivan Djukanovic <ivan.djukanovic07@gmail.com>
2022-12-11 13:16:32 +0100

    Reduced the font size of ride details component and now it shows distance of the route

--- src/app/components/ride-details/ride-details.component.ts
+++ src/app/components/ride-details/ride-details.component.ts
@@ -8,14 +8,11 @@ import { Component, Input, OnInit } from '@angular/core';
 export class RideDetailsComponent implements OnInit{
 
   @Input() destination: string = "Novi sad, Katolicka Porta 4";
-
   @Input() departure: string = "Novi sad, Katolicka Porta 4";
-
   @Input() departureDate: string = "16.11.2022.";
-
   @Input() departureTime: string = "15:30";
-
   @Input() duration: string = "15:36";
+  @Input() distance: string = "1000m";
 
   ngOnInit(): void {
 
