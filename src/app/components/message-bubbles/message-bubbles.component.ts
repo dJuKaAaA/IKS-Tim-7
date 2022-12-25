c00cec36e8292e2e9257ccd5e52597fc161e3b7b
Ivan Djukanovic <ivan.djukanovic07@gmail.com>
2022-12-25 13:52:40 +0100

    Chat component update
    Added dummy messages data for display. Added the ability to send
    messages. Div that contains messages now automatically scrolls down when
    a new message is sent.

--- src/app/components/message-bubbles/message-bubbles.component.ts
+++ src/app/components/message-bubbles/message-bubbles.component.ts
@@ -11,11 +11,57 @@ export class MessageBubblesComponent implements OnInit {
   @Input() messages: Array<Message> = [];
   dummyArray: Array<number> = []
   dummyMessageCount = 20;
+  senderId: number = 1;
+
+  typingMessageContent: string = "";
 
   ngOnInit() {
-    for (let i = 0; i < this.dummyMessageCount; ++i) {
-      this.dummyArray.push(i);
+    for (let i = 1; i <= this.dummyMessageCount; ++i) {
+      if (Math.random() > 0.5) {
+        this.messages.push({
+          id: i,
+          timeOfSending: new Date(),
+          senderId: 1,
+          receiverId: 2,
+          message: "Hello there :)",
+          type: "RIDE",
+          rideId: 1
+        })
+      } else {
+        this.messages.push({
+          id: i,
+          timeOfSending: new Date(),
+          senderId: 2,
+          receiverId: 1,
+          message: "General Kenobi >:)",
+          type: "RIDE",
+          rideId: 1
+        })
+      }
     }
+  }
+
+  sendMessage() {
+    if (this.typingMessageContent == "") { 
+      return;
+    }
+    const messageId: number = Math.floor(Math.random() * 10000);
+    console.log(messageId);
+    this.messages.push({
+      id: messageId,
+      timeOfSending: new Date(),
+      senderId: 1,
+      receiverId: 2,
+      message: this.typingMessageContent,
+      type: "RIDE",
+      rideId: 1
+    })
+    this.typingMessageContent = "";
+    setTimeout(() => {
+      const bubblesContainer = document.getElementsByClassName("bubbles-container")[0];
+      bubblesContainer.scrollTop = bubblesContainer.scrollHeight;
+    }, 300);
+    
   }
 
   
