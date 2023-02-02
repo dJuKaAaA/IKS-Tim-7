3e40cc092e1aca1f200a212788952614889ae81b
Ivan Djukanovic <ivan.djukanovic07@gmail.com>
2023-02-02 11:51:58 +0100

    Chart component refactor and admin charts
    - Created interfaces ChartDataModel as an object that the chart's
      datasets attribute takes
    - Created StatisticTotals interface that contains the attributes sum and
      total
    - Added methods for getting the instance of StatisticTotals based on the
      report response that is fetched from the backend
    - createChart method now takes multiple ChartDataModel objects for
      displaying chart data
    - Added a method that returns a ChartDataModel obj based on the report
      response
    - Refactored the show chart methods (made it prettier :D)
    - Added an email input field for admin (admin types the email and it
      shows data for that user)

--- src/app/components/user-statistics/user-statistics.component.ts
+++ src/app/components/user-statistics/user-statistics.component.ts
@@ -3,12 +3,26 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { throwToolbarMixedModesError } from '@angular/material/toolbar';
 import { Chart } from 'chart.js/auto';
+import { DriverListDTO } from 'src/app/model/driver-list-dto';
 import { CreateReport } from 'src/app/model/report/create-report.model';
 import { ResponseReport } from 'src/app/model/report/response-report.model';
 import { AuthService } from 'src/app/services/auth.service';
 import { DateTimeService } from 'src/app/services/date-time.service';
+import { DriverService } from 'src/app/services/driver.service';
+import { PassengerService } from 'src/app/services/passenger.service';
 import { StatisticService } from 'src/app/services/statistic.service';
 import { DialogComponent } from '../dialog/dialog.component';
+
+interface ChartDataModel {
+  label: string,
+  data: Array<string>,
+  backgroundColor: string
+}
+
+interface StatisticTotals {
+  sum: number,
+  average: number
+}
 
 @Component({
   selector: 'app-user-statistics',
@@ -18,24 +32,27 @@ import { DialogComponent } from '../dialog/dialog.component';
 export class UserStatisticsComponent implements OnInit {
 
   chart: any;
+
   financialsChartLabel: string = "";
   startDate: Date = new Date();
   endDate: Date = new Date();
 
   dateLabels: Array<string> = [];
-  chartData: Array<string> = [];
-  chartDataLabel: string = "";
 
-  totalValue: number = 0;
+  sumValue: number = 0;
   averageValue: number = 0;
+
+  userEmail: string = "";
 
   @ViewChild('MyChart') canvasRef: ElementRef;
 
   constructor(
-    private authService: AuthService,
+    public authService: AuthService,
     private statisticService: StatisticService,
     private dateTimeService: DateTimeService,
-    private matDialog: MatDialog
+    private matDialog: MatDialog,
+    private driverService: DriverService,
+    private passengerServie: PassengerService
   ) { }
 
   ngOnInit() {
@@ -43,30 +60,20 @@ export class UserStatisticsComponent implements OnInit {
       this.financialsChartLabel = "Expenses"
     } else if (this.authService.getRole() == "ROLE_DRIVER") {
       this.financialsChartLabel = "Income";
+    } else {
+      this.financialsChartLabel = "Financials";
     }
-    this.createChart('white');
+    this.createChart({ label: "", data: [], backgroundColor: 'white' });
   }
 
-  createChart(backgroundColor: string) {
+  createChart(...chartDataModel: Array<ChartDataModel>) {
 
     this.chart = new Chart("MyChart", {
-      type: 'bar', //this denotes tha type of chart
+      type: 'bar', //this denotes the type of chart
 
       data: {  // values on X-Axis
         labels: this.dateLabels,
-        datasets: [
-          {
-            label: this.chartDataLabel,
-            data: this.chartData,
-            backgroundColor: backgroundColor 
-          },
-          // {
-          //   label: "Profit",
-          //   data: ['542', '542', '536', '327', '17',
-          //     '0.00', '538', '541'],
-          //   backgroundColor: 'limegreen'
-          // }
-        ]
+        datasets: chartDataModel
       },
       options: {
         aspectRatio: 2.5
@@ -75,17 +82,35 @@ export class UserStatisticsComponent implements OnInit {
     });
   }
 
-  private setDataForChart(data: Array<ResponseReport>) {
-    this.averageValue = 0;
-    this.totalValue = 0;
-    this.dateLabels = []
-    this.chartData = [];
+  private getStatisticTotals(data: Array<ResponseReport>): StatisticTotals {
+    let sum: number = 0;
     for (let report of data) {
       this.dateLabels.push(report.date.split(" ")[0]);
-      this.chartData.push(`${report.value}`);
-      this.totalValue += report.value;
+      sum += report.value;
     }
-    this.averageValue = Math.round(this.totalValue / data.length * 100) / 100;
+    return {
+      sum: sum,
+      average: Math.round(sum / data.length * 100) / 100
+    }
+  }
+
+  private setChartDateLabels(data: Array<ResponseReport>) {
+    this.dateLabels = []
+    for (let report of data) {
+      this.dateLabels.push(report.date.split(" ")[0]);
+    }
+  }
+
+  private getChartDataModel(data: Array<ResponseReport>, label: string, backgroundColor: string): ChartDataModel {
+    let chartData: Array<string> = []
+    for (let report of data) {
+      chartData.push(`${report.value}`);
+    }
+    return {
+      data: chartData,
+      label: label,
+      backgroundColor: backgroundColor
+    }
   }
 
   private getCreateReportObject() {
@@ -95,70 +120,216 @@ export class UserStatisticsComponent implements OnInit {
     }
   }
 
-  renderChart(backgroundColor: string) {
+  renderChart(...chartDataModel: Array<ChartDataModel>) {
     this.chart.destroy();
-    this.createChart(backgroundColor);
+    this.createChart(...chartDataModel);
   }
 
   showDistanceTraveledChart() {
-    const createReport: CreateReport = this.getCreateReportObject();
-    this.statisticService.getDistanceTraveledPerDay(this.authService.getId(), createReport).subscribe({
-      next: (response: Array<ResponseReport>) => {
-        this.chartDataLabel = "Distance traveled";
-        this.setDataForChart(response);
-        this.renderChart('blue');
-      }, error: (error) => {
-        if (error instanceof HttpErrorResponse) {
-          this.matDialog.open(DialogComponent, {
-            data: {
-              header: "Error!",
-              body: error.error.message
-            }
-          });
-        }
-      }
-    })
+    if (this.authService.getRole() == "ROLE_ADMIN") {
+      this.userEmail = this.userEmail.trim();
+      this.showDistanceTraveledChartForAdmin();
+    } else {
+      this.showDistanceTraveledChartForRegularUser();
+    }
+  }
 
+  private showDistanceTraveledChartForAdmin() {
+    if (this.userEmail == "") {
+      this.allUsersDataDistanceTraveledChart();
+    } else {
+      const createReport: CreateReport = this.getCreateReportObject();
+      this.statisticService.getDistanceTraveledPerDayByEmail(this.userEmail, createReport).subscribe({
+        next: (response: Array<ResponseReport>) => {
+          const chartDataLabel: string = "Distance Traveled";
+          const backgroundColor = 'blue';
+
+          const statisticTotals = this.getStatisticTotals(response);
+          this.sumValue = statisticTotals.sum;
+          this.averageValue = statisticTotals.average;
+
+          this.setChartDateLabels(response);
+          this.renderChart(this.getChartDataModel(response, chartDataLabel, backgroundColor));
+        }, error: (error) => {
+          if (error instanceof HttpErrorResponse) {
+            this.matDialog.open(DialogComponent, {
+              data: {
+                header: "Error!",
+                body: error.error.message
+              }
+            });
+          }
+        }
+      })
+    }
+  }
+
+  private allUsersDataDistanceTraveledChart() {
+
+  }
+
+  private showDistanceTraveledChartForRegularUser() {
+    const createReport: CreateReport = this.getCreateReportObject();
+      this.statisticService.getDistanceTraveledPerDay(this.authService.getId(), createReport).subscribe({
+        next: (response: Array<ResponseReport>) => {
+          const chartDataLabel: string = "Distance Traveled";
+          const backgroundColor = 'blue';
+
+          const statisticTotals = this.getStatisticTotals(response);
+          this.sumValue = statisticTotals.sum;
+          this.averageValue = statisticTotals.average;
+
+          this.setChartDateLabels(response);
+          this.renderChart(this.getChartDataModel(response, chartDataLabel, backgroundColor));
+        }, error: (error) => {
+          if (error instanceof HttpErrorResponse) {
+            this.matDialog.open(DialogComponent, {
+              data: {
+                header: "Error!",
+                body: error.error.message
+              }
+            });
+          }
+        }
+      })
   }
 
   showNumberOfRidesChart() {
-    const createReport: CreateReport = this.getCreateReportObject();
-    this.statisticService.getNumberOfRidesPerDay(this.authService.getId(), createReport).subscribe({
-      next: (response: Array<ResponseReport>) => {
-        this.chartDataLabel = "Number of rides"  // put this in subscribe
-        this.setDataForChart(response);
-        this.renderChart('red');
-      }, error: (error) => {
-        if (error instanceof HttpErrorResponse) {
-          this.matDialog.open(DialogComponent, {
-            data: {
-              header: "Error!",
-              body: error.error.message
-            }
-          });
+    if (this.authService.getRole() == "ROLE_ADMIN") {
+      this.userEmail = this.userEmail.trim();
+      this.showNumberOfRidesChartForAdmin();
+    } else {
+      this.showNumberOfRidesChartForRegularUser();
+    }
+  }
+
+  private showNumberOfRidesChartForAdmin() {
+    if (this.userEmail.trim() == "") {
+      this.allUsersDataNumberOfRidesChart();
+    } else {
+      const createReport: CreateReport = this.getCreateReportObject();
+      this.statisticService.getNumberOfRidesPerDayByEmail(this.userEmail, createReport).subscribe({
+        next: (response: Array<ResponseReport>) => {
+          const chartDataLabel: string = "Number of Rides";
+          const backgroundColor = 'red';
+
+          const statisticTotals = this.getStatisticTotals(response);
+          this.sumValue = statisticTotals.sum;
+          this.averageValue = statisticTotals.average;
+
+          this.setChartDateLabels(response);
+          this.renderChart(this.getChartDataModel(response, chartDataLabel, backgroundColor));
+        }, error: (error) => {
+          if (error instanceof HttpErrorResponse) {
+            this.matDialog.open(DialogComponent, {
+              data: {
+                header: "Error!",
+                body: error.error.message
+              }
+            });
+          }
         }
-      }
-    })
+      })
+    }
+  }
+
+  private allUsersDataNumberOfRidesChart() {
+
+  }
+
+  private showNumberOfRidesChartForRegularUser() {
+    const createReport: CreateReport = this.getCreateReportObject();
+      this.statisticService.getNumberOfRidesPerDay(this.authService.getId(), createReport).subscribe({
+        next: (response: Array<ResponseReport>) => {
+          const chartDataLabel: string = "Number of Rides";
+          const backgroundColor = 'red';
+
+          const statisticTotals = this.getStatisticTotals(response);
+          this.sumValue = statisticTotals.sum;
+          this.averageValue = statisticTotals.average;
+
+          this.setChartDateLabels(response);
+          this.renderChart(this.getChartDataModel(response, chartDataLabel, backgroundColor));
+        }, error: (error) => {
+          if (error instanceof HttpErrorResponse) {
+            this.matDialog.open(DialogComponent, {
+              data: {
+                header: "Error!",
+                body: error.error.message
+              }
+            });
+          }
+        }
+      })
   }
 
   showFinancialsChart() {
+    if (this.authService.getRole() == "ROLE_ADMIN") {
+      this.userEmail = this.userEmail.trim();
+      this.showFinancialsChartForAdmin();
+    } else {
+      this.showFinancialsChartForRegularUser();
+    }
+  }
+
+  private showFinancialsChartForAdmin() {
+    if (this.userEmail.trim() == "") {
+      this.allUsersDataFinancialsChart();
+    } else {
     const createReport: CreateReport = this.getCreateReportObject();
-    this.statisticService.getFinancialsPerDay(this.authService.getId(), createReport).subscribe({
-      next: (response: Array<ResponseReport>) => {
-        this.chartDataLabel = this.financialsChartLabel  // put this in subscribe 
-        this.setDataForChart(response);
-        this.renderChart('green');
-      }, error: (error) => {
-        if (error instanceof HttpErrorResponse) {
-          this.matDialog.open(DialogComponent, {
-            data: {
-              header: "Error!",
-              body: error.error.message
-            }
-          });
+      this.statisticService.getFinancialsPerDayByEmail(this.userEmail, createReport).subscribe({
+        next: (response: Array<ResponseReport>) => {
+          const chartDataLabel: string = this.financialsChartLabel;
+          const backgroundColor = 'green';
+          
+          const statisticTotals = this.getStatisticTotals(response);
+          this.sumValue = statisticTotals.sum;
+          this.averageValue = statisticTotals.average;
+
+          this.setChartDateLabels(response);
+          this.renderChart(this.getChartDataModel(response, chartDataLabel, backgroundColor));
+        }, error: (error) => {
+          if (error instanceof HttpErrorResponse) {
+            this.matDialog.open(DialogComponent, {
+              data: {
+                header: "Error!",
+                body: error.error.message
+              }
+            });
+          }
         }
-      }
-    })
+      })
+  }
+    }
+
+  private allUsersDataFinancialsChart() {
+
+  }
+
+  private showFinancialsChartForRegularUser() {
+    const createReport: CreateReport = this.getCreateReportObject();
+      this.statisticService.getFinancialsPerDay(this.authService.getId(), createReport).subscribe({
+        next: (response: Array<ResponseReport>) => {
+          const chartDataLabel: string = this.financialsChartLabel;
+          const backgroundColor = 'green';
+          
+          const statisticTotals = this.getStatisticTotals(response);
+          this.sumValue = statisticTotals.sum;
+          this.averageValue = statisticTotals.average;
+
+          this.setChartDateLabels(response);
+          this.renderChart(this.getChartDataModel(response, chartDataLabel, backgroundColor));
+        }, error: (error) => {
+          if (error instanceof HttpErrorResponse) {
+            this.matDialog.open(DialogComponent, {
+              data: {
+                header: "Error!",
+                body: error.error.message
+              }
+            });
+          }
+        }
+      })
   }
 
 }
--- src/app/services/statistic.service.ts
+++ src/app/services/statistic.service.ts
@@ -23,4 +23,16 @@ export class StatisticService {
   public getFinancialsPerDay(userId: number, createReport: CreateReport): Observable<Array<ResponseReport>> {
     return this.http.post<Array<ResponseReport>>(environment.localhostApi + `statistic/financialsPerDay/${userId}`, createReport);
   }
+
+  public getDistanceTraveledPerDayByEmail(email: string, createReport: CreateReport): Observable<Array<ResponseReport>> {
+    return this.http.post<Array<ResponseReport>>(environment.localhostApi + `statistic/traveledDistancePerDayByEmail/${email}`, createReport);
+  }
+
+  public getNumberOfRidesPerDayByEmail(email: string, createReport: CreateReport): Observable<Array<ResponseReport>> {
+    return this.http.post<Array<ResponseReport>>(environment.localhostApi + `statistic/numberOfRidesPerDayByEmail/${email}`, createReport);
+  }
+
+  public getFinancialsPerDayByEmail(email: string, createReport: CreateReport): Observable<Array<ResponseReport>> {
+    return this.http.post<Array<ResponseReport>>(environment.localhostApi + `statistic/financialsPerDayByEmail/${email}`, createReport);
+  }
 }
